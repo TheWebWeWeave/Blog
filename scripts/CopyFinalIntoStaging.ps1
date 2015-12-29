261b87db511385db5ded9f4dff1a770bc2053c98
Donald Schulz <dschulz@notionsolutions.com>
2015-12-29 15:27:53 -0800

    Updated the PS Script and the ingnore file now that we have some dot net code here.

--- scripts/CopyFinalIntoStaging.ps1
+++ scripts/CopyFinalIntoStaging.ps1
@@ -5,6 +5,7 @@
 
 $targetPath = "$StagingPath"
 $scriptPath = "$StagingPath\Scripts"
+$testPath = "$StagingPath\Tests"
 
 Remove-Item $StagingPath -Recurse -Force
 
@@ -16,6 +17,11 @@ if (-not(Test-Path($scriptPath))) {
         mkdir $scriptPath
     }
 
+if (-not(Test-Path($testPath))) {
+        mkdir $testPath
+    }
+
 Copy-Item "$SourcePath\public" -Recurse -Destination $targetPath -Force
 Copy-Item "$SourcePath\blog.zip" -Destination $StagingPath -Force
 Copy-Item "$SourcePath\scripts\*.*" -Destination $scriptPath -Force
+Copy-Item "$SourcePath\DeployViaFTP\bin\Debug\*.*" -Destination $testPath -Force
