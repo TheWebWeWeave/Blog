d5cf78d7a1a9755d73de539cc1d5e5a265e4ad09
Donald Schulz <dschulz@notionsolutions.com>
2015-12-28 23:25:30 -0800

    Added the CopyFinalIntoStaging script to prepare for the drop

---
+++ scripts/CopyFinalIntoStaging.ps1
@@ -0,0 +1,24 @@
+﻿param(
+    [string]$SourcePath, 
+    [string]$StagingPath
+)
+
+$targetPath = "$StagingPath\DoS"
+$scriptPath = "$targetPath\Scripts"
+$testPath = "$targetPath\Tests"
+
+if (-not(Test-Path($targetPath))) {
+        mkdir $targetPath
+    }
+
+if (-not(Test-Path($scriptPath))) {
+        mkdir $scriptPath
+    }
+
+if (-not(Test-Path($testPath))) {
+        mkdir $testPath
+    }
+
+Copy-Item "$SourcePath\public\*.*" -Destination $targetPath
+Copy-Item "$SourcePath\blog.zip -Destination $StagingPath
+Copy-Item "$SourcePath\scripts\*.*" -Destination $scriptPath
