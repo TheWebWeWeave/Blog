b6a2b0880164ca33a8d55be88eeb434d0aa219a8
Donald Schulz <Donald.Schulz@wwt.com>
2020-07-08 14:52:40 -0700

    have a temperary fix for tagging of the master branch image

--- build/ci/02-build.sh
+++ build/ci/02-build.sh
@@ -1,2 +1,2 @@
 # docker-compose -f docker-compose.yml build --pull
-docker image build --rm -f ./build/Dockerfile -t t3winc/donaldonsoftware:1.11.0-alpha1 .
+docker image build --rm -f ./build/Dockerfile -t t3winc/donaldonsoftware .
--- build/ci/03-publish.sh
+++ build/ci/03-publish.sh
@@ -4,5 +4,5 @@
 
 #This should send the image up to github packages...
 cat /var/jenkins_home/GH_TOKEN.txt | docker login docker.pkg.github.com -u SchulzDL --password-stdin
-docker tag IMAGE_ID docker.pkg.github.com/t3winc/blog/donaldonsoftware:1.11.0
+docker tag $(docker images | awk '{print $3}' | awk 'NR==2') docker.pkg.github.com/t3winc/blog/donaldonsoftware:1.11.0
 docker push docker.pkg.github.com/t3winc/blog-name/donaldonsoftware:1.11.0
