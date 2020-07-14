7afcd2ede074c9663cf56d657c1513db19de6f46
Donald Schulz <Donald.Schulz@wwt.com>
2020-07-14 13:46:21 -0700

    implemented our first conditional stages to control both release candidates and final releases on the same pipeline

--- build/ci/03-publish.sh
+++ build/ci/03-publish.sh
@@ -1,7 +1,3 @@
-#This should send the image up to docker hub...
-#cat /var/jenkins_home/my_password.txt | docker login --username schulzdl --password-stdin
-#docker push t3winc/donaldonsoftware:1.11.0-alpha1
-
 #This should send the image up to github packages...
 cat /var/jenkins_home/GH_TOKEN.txt | docker login docker.pkg.github.com -u SchulzDL --password-stdin
 docker tag $(docker images | awk '{print $3}' | awk 'NR==2') docker.pkg.github.com/t3winc/blog/donaldonsoftware:$1
