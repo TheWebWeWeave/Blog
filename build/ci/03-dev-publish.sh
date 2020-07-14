bed11ca5e5f89cbf20bcbb840a8c331290d40e62
Donald Schulz <Donald.Schulz@wwt.com>
2020-07-14 16:02:59 -0700

    changed the names to make it easier to understand

--- build/ci/03-dev-publish.sh
+++
@@ -1,4 +0,0 @@
-#This should send the image up to docker hub...
-cat /var/jenkins_home/my_password.txt | docker login --username schulzdl --password-stdin
-docker tag $(docker images | awk '{print $3}' | awk 'NR==2') t3winc/donaldonsoftware:$1
-docker push t3winc/donaldonsoftware:$1
