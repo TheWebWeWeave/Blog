6ad13205f1b35087a523c1c9f135041394625717
Donald Schulz <Donald.Schulz@wwt.com>
2020-07-31 14:59:27 -0700

    fixed up the pipeline scripts so that it can still work with all the changes I made

--- build/ci/01-version.sh
+++ build/ci/01-version.sh
@@ -1,4 +1,6 @@
 #docker run --rm --volume "$(pwd):/repo" gittools/gitversion:5.3.4-linux-alpine.3.10-x64-netcoreapp3.1 /repo -output json
 echo "We are here: ${pwd}"
 echo "The correct version number: ${semver}"
-#echo "The branch is: ${env.GIT_BRANCH}"
+echo "The correct branch is: ${branchname}"
+echo "The git_branch says: ${env.GIT_BRANCH}"
+git checkout ${branchname}
--- build/ci/03-publish.sh
+++ build/ci/03-publish.sh
@@ -1,4 +1,4 @@
 #This should send the image up to github packages...
 cat /var/jenkins_home/GH_TOKEN.txt | docker login docker.pkg.github.com -u SchulzDL --password-stdin
-docker tag $(docker images | awk '{print $3}' | awk 'NR==2') docker.pkg.github.com/t3winc/blog/donaldonsoftware:$1
-docker push docker.pkg.github.com/t3winc/blog/donaldonsoftware:$1
+docker tag $(docker images | awk '{print $3}' | awk 'NR==2') docker.pkg.github.com/thewebweweave/blog/donaldonsoftware:$1
+docker push docker.pkg.github.com/thewebweweave/blog/donaldonsoftware:$1
