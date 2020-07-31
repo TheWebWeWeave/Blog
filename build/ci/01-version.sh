cf07ba3cb0487e4790b584f58cca496083e435c4
Donald Schulz <Donald.Schulz@wwt.com>
2020-07-31 15:31:53 -0700

    updated the shell scripts and compose files to the name change and additional parameter

--- build/ci/01-version.sh
+++ build/ci/01-version.sh
@@ -1,4 +1,7 @@
 #docker run --rm --volume "$(pwd):/repo" gittools/gitversion:5.3.4-linux-alpine.3.10-x64-netcoreapp3.1 /repo -output json
 echo "We are here: ${pwd}"
 echo "The correct version number: ${semver}"
-#echo "The branch is: ${env.GIT_BRANCH}"
+echo "The actual branch is: ${branchname}"
+echo "The git_branch is: ${env.GIT_BRANCH}"
+
+git checkout "${branchname}
