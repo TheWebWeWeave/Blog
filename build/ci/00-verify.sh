15b649c8257313f475566d429ea452d5c9236f9d
Donald Schulz <Donald.Schulz@wwt.com>
2020-07-31 18:04:27 -0700

    does not like the git command

--- build/ci/00-verify.sh
+++ build/ci/00-verify.sh
@@ -1,4 +1,4 @@
 docker version && docker-compose version
 echo "my semver version is: ${semver}"
-echo "current git_branch is: ${GIT_BRANCH}"
+#echo "current git_branch is: ${GIT_BRANCH}"
 echo "actual branch name is: ${branchname}"
