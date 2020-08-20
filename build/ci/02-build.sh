4ed8a5d60a3c8cb4e28cb4d6b2364dca922346d1
Donald Schulz <Donald.Schulz@wwt.com>
2020-08-20 10:37:24 -0700

    just changed the way variables are passed

--- build/ci/02-build.sh
+++ build/ci/02-build.sh
@@ -1,2 +1,2 @@
 # docker-compose -f docker-compose.yml build --pull
-docker image build --rm -f ./build/Dockerfile -t t3winc/donaldonsoftware .
+docker image build --rm -f ./build/Dockerfile -t donaldonsoftware .
