064c6902d0d807baf32885beb4a3a504ab5d5cea
Donald Schulz <Donald.Schulz@wwt.com>
2020-10-20 14:18:31 -0700

    moved the + symbol one character closer to the format string

--- build/ci/00-docker-clean.sh
+++ build/ci/00-docker-clean.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 log=/var/lib/docker/prune.log
-date + '=== %Y.%m.%d %H:%M ===' >> $log
+date +'=== %Y.%m.%d %H:%M ===' >> $log
 docker system prune -af --filter "until=$((7*24))h" >> $log
 # docker rm $(docker ps --no-trunc -aq)
 # docker rmi $(docker images -q)
