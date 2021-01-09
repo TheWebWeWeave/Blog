a84b24e04f366a1de7128c466f75f1e1060ced79
Donald Schulz <donald@thewebweweave.com>
2021-01-09 09:49:35 -0800

    fixed the name of the actual image

--- build/ci/02-testImage.sh
+++ build/ci/02-testImage.sh
@@ -1,4 +1,4 @@
 i=0
-container-structure-test test --image t3winc/donaldonsoftware:$1 --config ./test/DockerTest/unit-test.yaml || ((i++))
+container-structure-test test --image donaldonsoftware --config ./test/DockerTest/unit-test.yaml || ((i++))
 
 exit $i
