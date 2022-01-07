7bedab7e1cff73fa8630f63c54cc1fe34140d6fc
Donald Schulz <donald@thewebweweave.com>
2022-01-06 18:56:28 -0800

    the first round of tests are working

---
+++ test/Selenium/blog_xunit/DriverFixture.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace blog_xunit
+{
+    public class DriverFixture : IDisposable
+    {
+        RemoteWebDriver driver;
+        public DriverFixture()
+        {
+            var chromeOption = new ChromeOptions();
+            chromeOption.AddAdditionalOption("se:recordVideo", true);
+            chromeOption.AddArgument("--disable-dev-shm-usage");
+            driver = new RemoteWebDriver(new Uri("http://localhost:4444/"), chromeOption);
+        }
+
+        public RemoteWebDriver Driver => driver;
+
+        public void Dispose()
+        {
+            Driver.Quit();
+        }
+    }
+}
--- test/Selenium/blog_xunit/UnitTest1.cs
+++ test/Selenium/blog_xunit/UnitTest1.cs
@@ -4,12 +4,44 @@ using Xunit;
 
 namespace blog_xunit;
 
-public class UnitTest1
+public class UnitTest1 : IClassFixture<DriverFixture>
 {
+    private readonly DriverFixture driverFixture;
+
+    public UnitTest1(DriverFixture driverFixture)
+    {
+        this.driverFixture = driverFixture;
+    }
+
+    [Fact]
+    public void Test1()
+    {
+        driverFixture.Driver.Navigate().GoToUrl("http://localhost:1313/");
+    }
+
+    [Fact]
+    public void AboutPageIsReachable()
+    {
+        driverFixture.Driver.Navigate().GoToUrl("https://blog.t3winc.com/");
+        driverFixture.Driver.FindElementByClassName("navbar-burger").Click();
+        driverFixture.Driver.FindElementByXPath("//body/nav[1]/div[2]/div[1]/a[2]").Click();
+        var postTitle = driverFixture.Driver.FindElementByClassName("title");
+
+        Assert.Equal("About Donald on Software", postTitle.Text);
+    }
+
     [Theory]
     [CsvData("./Parameters.csv")]
-    public void TestWithCSVData(string Category, string Post_Title, string Archive_Year)
+    public void PageExitsFromArchive(string Category, string Post_Title, string Archive_Year)
     {
+        driverFixture.Driver.Navigate().GoToUrl("https://blog.t3winc.com/");
+
         Debug.WriteLine($"{Category} - {Post_Title} - {Archive_Year}");
+
+        driverFixture.Driver.FindElementByLinkText(Archive_Year).Click();
+        driverFixture.Driver.FindElementByLinkText(Post_Title).Click();
+        var postTitle = driverFixture.Driver.FindElementByCssSelector("h1.title");
+
+        Assert.Equal(Post_Title, postTitle.Text);
     }
 }
