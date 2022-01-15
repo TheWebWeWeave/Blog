7a0f0035099332a48a181565920b2f6e3c7803e4
Donald Schulz <donald@thewebweweave.com>
2022-01-15 15:02:10 -0800

    Just added an additional constructor with arguments

--- test/Selenium/blog_xunit/DriverFixture.cs
+++ test/Selenium/blog_xunit/DriverFixture.cs
@@ -1,5 +1,8 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Safari;
 using OpenQA.Selenium.Remote;
 using System;
 using System.Collections.Generic;
@@ -21,12 +24,51 @@ namespace blog_xunit
             driver = new RemoteWebDriver(new Uri("https://testing.t3winc.com/"), chromeOption);          
         }
 
-        public RemoteWebDriver Driver => driver;
+        public DriverFixture(BrowserType browserType)
+        {
+            var browserOption = GetBrowserOptions(browserType);
+            browserOption.AddArgument("--disable-dev-shm-usage");
+            driver = new RemoteWebDriver(new Uri("https://testing.t3winc.com/"), browserOption);
+        }
 
+        public RemoteWebDriver Driver => driver;
 
         public void Dispose()
         {
             Driver.Quit();
         }
+
+        private dynamic GetBrowserOptions(BrowserType browserType)
+        {
+            switch (browserType)
+            {
+                case BrowserType.Chrome:
+                    {
+                        var chromeOption = new ChromeOptions();
+                        chromeOption.AddAdditionalOption("se:recordVideo", true);
+                        return chromeOption;
+                    }
+                case BrowserType.Firefox:
+                    {
+                        var firefoxOption = new FirefoxOptions();
+                        firefoxOption.AddAdditionalOption("se:recordVideo", true);
+                        return firefoxOption;
+                    }
+                case BrowserType.Safari:
+                    return new SafariOptions();
+                case BrowserType.Edge:
+                    return new EdgeOptions();
+                default:
+                    return new ChromeOptions();
+            }
+        }
+    }
+
+    public enum BrowserType 
+    {
+        Chrome,
+        Firefox,
+        Safari,
+        Edge
     }
 }
